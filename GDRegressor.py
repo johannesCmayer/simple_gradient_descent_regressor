3d93f4a40d2eee9dc0a85a45c3309b0f3176616f
Johannes C. Mayer <c@johannesCmayer.com>
2018-01-06 10:59:23 +0100

    initial commit

---
+++ GDRegressor.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def step_gradient(b, m, points, learning_rate):
+    #gradient descent
+    pass
+
+def gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_of_iterations):
+    for i in range(num_of_iterations):
+        b, m = step_gradient(b, m, array(points), learning_rate)
+    return [b, m]
+
+def run ():con
+    points = np.genfromtxt('data.csv', delimiter=',')
+    # Hyperparameter
+    learning_rate = 0.0001
+
+    initial_b = 0
+    initial_m = 0
+    num_of_iterations = 1000
+
+    gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_of_iterations)
+
+if __name__ == 'main':
+    run()
